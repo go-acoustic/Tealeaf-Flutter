3988266e08889b2b7b0d8c35394acb0c9e28dc31
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2023-03-03 09:33:11 -0800

    1.1.0  402

---
+++ lib/src/plugins/aop/aop.dart
@@ -0,0 +1,9 @@
+// @dart=2.12
+
+export 'annotation/aspect.dart';
+export 'annotation/call.dart';
+export 'annotation/execute.dart';
+export 'annotation/inject.dart';
+export 'annotation/pointcut.dart';
+export 'extended_annotation/add.dart';
+export 'extended_annotation/field_get.dart';
